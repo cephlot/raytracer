2bb4238f07c9bb3769d7a1ba62db710df7e351d3
ceph <c18oln@cs.umu.se>
2021-12-28 12:14:26 +0100

    [update] Implements rest of Canvas

--- src/graphics/canvas.rs
+++ src/graphics/canvas.rs
@@ -1,5 +1,6 @@
 use super::color;
 
+#[derive(Debug, Clone)]
 pub struct Canvas {
     pub width: usize,
     pub height: usize,
@@ -21,11 +22,74 @@ impl Canvas {
         height: usize, 
         color: color::Color
     ) -> () {
-        self.pixels[width-1][height-1] = color;
+        self.pixels[height][width] = color;
     }
 
     pub fn pixel_at(&self, width: usize, height: usize) -> color::Color {
-        self.pixels[width-1][height-1]
+        self.pixels[height][width]
+    }
+
+    pub fn to_ppm(&self) -> String {
+        let mut s = String::from(
+            format!("P3\n{} {}\n255", self.width, self.height)
+        );
+
+        for i in &self.pixels {
+            s.push('\n');
+
+            let mut z = String::new();
+
+            for j in i {
+                let st = &format!("{}", clamp(j.r)).to_owned();
+                
+                if z.len() + st.len() >=70 {
+                    z.remove(z.len()-1);
+                    z.push('\n');
+                    s.push_str(&z.to_owned());
+                    z = String::new();
+                }
+
+                z.push_str(&format!("{} ", st));
+
+                let st = &format!("{}", clamp(j.g)).to_owned();
+
+                if z.len() + st.len() >=70 {
+                    z.remove(z.len()-1);
+                    z.push('\n');
+                    s.push_str(&z.to_owned());
+                    z = String::new();
+                }
+                
+                z.push_str(&format!("{} ", st));
+
+                let st = &format!("{}", clamp(j.b)).to_owned();
+
+                if z.len() + st.len() >=70 {
+                    z.remove(z.len()-1);
+                    z.push('\n');
+                    s.push_str(&z.to_owned());
+                    z = String::new();
+                }
+                
+                z.push_str(&format!("{} ", st));
+            }
+
+            z.remove(z.len()-1);
+            s.push_str(&z.to_owned());
+        }
+        
+        s.push('\n');
+        s
+    }
+}
+
+fn clamp(v: f64) -> u8 {
+    if v < 0.0 {
+        0
+    } else if v > 1.0 {
+        255
+    } else {
+        (v*(255 as f64)).ceil() as u8
     }
 }
 
@@ -50,6 +114,49 @@ mod tests {
         c.write_pixel(2, 3, red);
 
         assert_eq!(red, c.pixel_at(2, 3));
+    }
+
+    #[test]
+    fn should_construct_ppm_header() {
+        let c = Canvas::new(5, 3);
+
+        assert_eq!(true, c.to_ppm().contains("P3\n5 3\n255"));
+    }
+
+    #[test]
+    fn should_construct_ppm_pixel_data_correctly() {
+        let mut d = Canvas::new(5, 3);
+        let a = color::Color::new(1.5, 0.0, 0.0);
+        let b = color::Color::new(0.0, 0.5, 0.0);
+        let c = color::Color::new(-0.5, 0.0, 1.0);
+
+        d.write_pixel(0, 0, a);
+        d.write_pixel(2, 1, b);
+        d.write_pixel(4, 2, c);
+
+        assert_eq!(true, d.to_ppm().contains("255 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n0 0 0 0 0 0 0 128 0 0 0 0 0 0 0\n0 0 0 0 0 0 0 0 0 0 0 0 0 0 255"));
+    }
+
+    #[test]
+    fn should_not_be_longer_than_70_characters() {
+        let mut c = Canvas::new(10, 2);
+
+        for i in 0..10 {
+            for j in 0..2 {
+                c.write_pixel(i, j, color::Color::new(1.0, 0.8, 0.6));
+            }
+        }
+
+        eprintln!("{}", c.to_ppm());
+
+        assert_eq!(true, c.to_ppm().contains("255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204\n153 255 204 153 255 204 153 255 204 153 255 204 153\n255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204\n153 255 204 153 255 204 153 255 204 153 255 204 153"));
+    }
+
+    #[test]
+    fn should_contain_newline_as_last_character() {
+        let c = Canvas::new(5, 3);
+
+        assert_eq!('\n', c.to_ppm().chars().last().unwrap());
     }
 }
 
