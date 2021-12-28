1b70356d93f6dd4147a0406e5e9698e64232b934
ceph <c18oln@cs.umu.se>
2021-12-28 12:35:46 +0100

    [update] Implements .ppm ballistics example
    Writes to a static file 'ballistics.ppm' an image containing the
    trajectory of the projectile.

--- src/bin/bin.rs
+++ src/bin/bin.rs
@@ -1,21 +1,33 @@
-use raylib::{math::fundamentals::Tuple, Environment, Projectile};
+//! Binary example
+
+use std::fs;
+use raylib::{math::fundamentals::Tuple, Environment, Projectile, graphics};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut p = Projectile {
         position: Tuple::point(0.0, 1.0, 0.0),
-        velocity: Tuple::vector(1.0, 1.0, 0.0).normalize(),
+        velocity: Tuple::vector(1.0, 1.8, 0.0).normalize()*11.25,
     };
     let e = Environment {
         gravity: Tuple::vector(0.0, -0.1, 0.0),
         wind: Tuple::vector(-0.01, 0.0, 0.0),
     };
     let mut i = 0;
+    let mut c = graphics::canvas::Canvas::new(900, 550);
+    let red = graphics::color::Color::new(1.0, 0.0, 0.0);
 
     while p.position.y > 0.0 {
         p = e.tick(p);
         i += 1;
         println!("{}: {:?}", i, p.position);
+        c.write_pixel(
+            ((p.position.x).ceil() as usize)-1, 
+            (550-((p.position.y).ceil() as usize))-1, 
+            red
+        );
     }
+
+    fs::write("ballistics.ppm", c.to_ppm())?;
 
     Ok(())
 }
