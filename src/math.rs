24988000e20ba2fe8c97f85d587617334ee3f439
ceph <c18oln@cs.umu.se>
2022-01-07 23:55:30 +0100

    [update] Exposes intersection functionality

--- src/math.rs
+++ src/math.rs
@@ -2,11 +2,12 @@
 //!
 //! Contains fundamental math type and methods.
 
+mod intersection;
 mod matrix;
 mod transformations;
 mod tuple;
-mod ray;
 
+pub use intersection::{Intersection, Ray, Sphere};
 pub use matrix::Matrix;
 pub use transformations::*;
 pub use tuple::Tuple;
